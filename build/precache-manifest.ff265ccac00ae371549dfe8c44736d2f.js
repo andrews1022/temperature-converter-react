161f77a02850cafb62c25bbe4cb6cc49047916f2
andrews1022 <andrewshearerphotography@gmail.com>
2020-09-03 22:11:59 -0700

    production build, setting up for netlify hosting

---
+++ build/precache-manifest.ff265ccac00ae371549dfe8c44736d2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0494c38c9d4ae3ac268e0b43850f14bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d439843728b865ff7a2",
+    "url": "/static/css/main.01199cbc.chunk.css"
+  },
+  {
+    "revision": "442b3d58d70a8b794136",
+    "url": "/static/js/2.980bcfeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.980bcfeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d439843728b865ff7a2",
+    "url": "/static/js/main.1f75f6b1.chunk.js"
+  },
+  {
+    "revision": "267d0aa7f4a880a78db6",
+    "url": "/static/js/runtime-main.63395115.js"
+  }
+]);
